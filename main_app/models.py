a1d9ec16fcb4b96a91678757234bb3752f91b453
julianavalenti <julianabmartinez2@icloud.com>
2023-04-25 12:09:39 -0400

    adds db connection

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    base = models.CharField(max_length=100)
+    ingredients = models.TextField(max_length=350)
+
+    def __str__(self):
+        return self.name
