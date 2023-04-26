e39e3e9b347a9edd866dd1e99f7f69adf8084f77
julianavalenti <julianabmartinez2@icloud.com>
2023-04-26 09:23:48 -0400

    add few changes

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from.models import Drink 
+from.models import Drink, Trying 
 admin.site.register(Drink)
 # Register your models here.
+admin.site.register(Trying)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.urls import reverse
 
+TRY = (
+    ('N', 'No'),
+    ('Y', 'Yes')
+)
 # Create your models here.
 class Drink(models.Model):
     name = models.CharField(max_length=100)
@@ -8,7 +12,24 @@ class Drink(models.Model):
     ingredients = models.TextField(max_length=350)
 
     def __str__(self):
-        return self.name
+            return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'drink_id': self.id})
+            return reverse('detail', kwargs={'drink_id': self.id})
+
+class Trying(models.Model):
+    date = models.DateField()
+    tried = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=TRY,
+    # set the default value for meal to be 'B'
+    default=TRY[0][0])
+    
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_try_display()} on {self.date}"
+
+    
